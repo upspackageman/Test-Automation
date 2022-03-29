73e383af28bf189835ba8190a88633d0ce8ee963
William Ward <wward246@gmail.com>
2022-03-29 16:03:14 -0700

    Drivers
    Drivers

--- Automation Project/AutomationExtensions/Components/WebDriverFactory.cs
+++ Automation Project/AutomationExtensions/Components/WebDriverFactory.cs
@@ -1,6 +1,11 @@
 ﻿using Automation.Extensions.Contracts;
 using Newtonsoft.Json;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
 
 namespace Automation.Extensions.Components
 {
@@ -12,6 +17,61 @@ namespace Automation.Extensions.Components
         public WebDriverFactory(DriverParams driverParams)
         {
             _driverParams = driverParams;
+            if (string.IsNullOrEmpty(driverParams.Binaries) || driverParams.Binaries ==".")
+            {
+                _driverParams.Binaries = Environment.CurrentDirectory;
+            }
+        }
+
+        //Genereates web-driver based on input parameter
+        //return webdriver instance
+
+        public IWebDriver Get()
+        {
+            if (!string.Equals(_driverParams.Source, "REMOTE", StringComparison.OrdinalIgnoreCase))
+            {
+                return GetDriver();
+            }
+            return GetRemoteDriver();
+        }
+
+        //local Webdrivers
+        private IWebDriver GetChrome() => new ChromeDriver(_driverParams.Binaries);
+
+        private IWebDriver GetFirefox() => new FirefoxDriver(_driverParams.Binaries);
+
+        private IWebDriver GetEdge() => new EdgeDriver(_driverParams.Binaries);
+
+        private IWebDriver GetDriver()
+        {
+            switch (_driverParams.Driver.ToUpper())
+            {
+                case "EDGE": return GetEdge();
+                case "FIREFOX": return GetFirefox();
+                case "CHROME": return GetChrome();
+                default:  return GetChrome();
+            }
+            
+        }
+
+        //remote webdrivers
+
+        private IWebDriver GetRemoteChrome() => new RemoteWebDriver(new Uri(_driverParams.Binaries), new ChromeOptions());
+
+        private IWebDriver GetRemoteFirefox() => new RemoteWebDriver(new Uri(_driverParams.Binaries), new FirefoxOptions());
+
+        private IWebDriver GetRemoteEdge() => new RemoteWebDriver(new Uri(_driverParams.Binaries), new EdgeOptions());
+
+        private IWebDriver GetRemoteDriver()
+        {
+            switch (_driverParams.Driver.ToUpper())
+            {
+                case "EDGE": return GetRemoteEdge();
+                case "FIREFOX": return GetRemoteFirefox();
+                case "CHROME": return GetRemoteChrome();
+                default: return GetRemoteChrome();
+            }
+
         }
 
         private static DriverParams LoadParams(string driverParamsJson)
--- Automation Project/Autoumation.Testing/UnitTest1.cs
+++ Automation Project/Autoumation.Testing/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Automation.Extensions.Components;
+using Automation.Extensions.Contracts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -10,7 +12,7 @@ namespace Autoumation.Testing
 {
     [TestClass]
     public class UnitTest1
-    { 
+    {
         //[TestMethod]
         //public void WebDriver()
         //{
@@ -27,25 +29,42 @@ namespace Autoumation.Testing
         //    driver.Dispose();
         //}
 
+        //[TestMethod]
+        //public void WebElement()
+        //{
+        //    var driver = new ChromeDriver();
+        //    driver.Manage().Window.Maximize();
+
+        //    driver.Navigate().GoToUrl("https://www.mariedrake.com/post/api-testing-with-cypress");
+        //    Thread.Sleep(3000);
+        //    driver.FindElement(By.XPath("/html/body/div/div/div[3]/div/header/div/div[2]/div[2]/div/div/wix-dropdown-menu/nav/ul/li[3]/a/div/div/p")).Click();
+        //    Thread.Sleep(10000);
+        //    driver.Dispose();
+
+        //}
+
+
+        //[TestMethod]
+        //public void SelectElement()
+        //{
+        //    var driver = new ChromeDriver();
+        //    driver.Manage().Window.Maximize();
+
+        //    driver.Navigate().GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Course");
+        //    Thread.Sleep(3000);
+        //    var elem = driver.FindElement(By.XPath("//*[@id='SelectedDepartment']"));
+        //    var selectElement = new SelectElement(elem);
+        //    selectElement.SelectByValue("1");
+
+        //    Thread.Sleep(10000);
+        //    driver.Dispose();
+
+        //}
+
         [TestMethod]
-        public void WebElement()
+        public void WebDriverFactoryTest()
         {
-            var driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-
-            driver.Navigate().GoToUrl("https://www.mariedrake.com/post/api-testing-with-cypress");
-            Thread.Sleep(3000);
-            driver.FindElement(By.XPath("/html/body/div/div/div[3]/div/header/div/div[2]/div[2]/div/div/wix-dropdown-menu/nav/ul/li[3]/a/div/div/p")).Click();
-            Thread.Sleep(10000);
-            driver.Dispose();
-
-        }
-
-
-        [TestMethod]
-        public void SelectElement()
-        {
-            var driver = new ChromeDriver();
+            var driver = new WebDriverFactory(new DriverParams { Driver = "edge", Binaries = @"C:\Users\William Ward\source\repos\Test-Automation\Automation Project\web-driver" }).Get();
             driver.Manage().Window.Maximize();
 
             driver.Navigate().GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Course");
@@ -56,7 +75,6 @@ namespace Autoumation.Testing
 
             Thread.Sleep(10000);
             driver.Dispose();
-
         }
     }
 }
